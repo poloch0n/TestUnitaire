edfdd2f69f13c7e7678dd2f74b21064980402b71
poloch0n <50014021+poloch0n@users.noreply.github.com>
2019-05-23 11:05:13 +0200

    first commit

---
+++ tp01/logging/src/main/java/dev/logging/App.java
@@ -0,0 +1,15 @@
+package dev.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+		LOG.info("Bonjour Slf4J !");
+		LOG.info("Implémentation Logback");
+	}
+}
